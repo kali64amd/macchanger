97b97b53cba63bf78772603246a2e51008d53552
kali64amd <kali64amd@gmail.com>
2023-04-12 23:44:58 +0000

    Add files via upload

---
+++ macchanger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo ifconfig wlo1 down
+sudo macchanger -r wlo1
+sudo macchanger -r wlo1
+sudo macchanger -r wlo1
+sudo macchanger -r wlo1
+sudo macchanger -r wlo1
+sudo macchanger -r wlo1
+sudo macchanger -r wlo1
+sudo macchanger -r wlo1
+sudo macchanger -r wlo1
+sudo macchanger -r wlo1
+sudo ifconfig wlo1 up
+
